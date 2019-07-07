6121827d961a612e81e28e809a0ca969658d954e
feycot <feycot@gmail.com>
2019-07-07 16:31:34 +0500

    add properties

--- app/Gamedata/Account.php
+++ app/Gamedata/Account.php
@@ -6,6 +6,15 @@ use App\GamedataModel;
 
 /**
  * @property string $table
+ * @property string $login
+ * @property string $password
+ * @property int $lastactive
+ * @property int $accessLevel
+ * @property string $lastIP
+ * @property int  $lastServerId
+ * @property int $allowed_ip
+ * @property int $allowed_hwid
+ * @method mixed whereLogin($login)
  */
 class Account extends GamedataModel
 {
